efa70f0d424efd5f1640628ae9ca21356fdfd3e9
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2021-03-23 21:53:47 +0300

    fix

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -3,12 +3,12 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static async load() {
-      try {
-          const data = await read();
-          const value = await json(data);
-          return value;
-      } catch (error) {
-          return 'error';
-      }
+    try {
+      const data = await read();
+      const value = await json(data);
+      return value;
+    } catch (error) {
+      return error;
+    }
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import GameSavingLoader from './GameSavingLoader';
 
 (async () => {
   try {
-    const saving =  await GameSavingLoader.load();
+    const saving = await GameSavingLoader.load();
     console.log(saving);
   } catch (error) {
     console.log(error);
