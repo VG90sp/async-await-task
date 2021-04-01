1beade313f39a2639c464823816bf4c7df7b7275
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2021-04-01 21:25:55 +0300

    fix

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,12 +1,13 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static async load() {
     try {
       const data = await read();
       const value = await json(data);
-      return value;
+      return JSON.stringify(new GameSaving(value));
     } catch (error) {
       return error;
     }
